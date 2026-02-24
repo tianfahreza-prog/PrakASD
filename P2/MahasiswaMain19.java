37baf4c5f6c14829dbf2e03495e6aec81ac6ada6
tianfahreza-pro <tianfahreza240@gmail.com>
2026-02-24 13:20:41 +0700

    Percobaan3_Pertanyaan2

---
+++ P2/MahasiswaMain19.java
@@ -0,0 +1,22 @@
+package P2;
+public class MahasiswaMain19 {
+    public static void main(String[] args) {
+
+        Mahasiswa19 mhs1 = new Mahasiswa19();
+
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "2241720171";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIpk(3.60);
+        mhs1.tampilkanInformasi();
+
+        Mahasiswa19 mhs2 = new Mahasiswa19 ("Annisa Nabila", "2141720160", 3.25, "TI 2L") ;
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi ();
+    }
+
+}
