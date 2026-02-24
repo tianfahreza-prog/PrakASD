563708a1c9c4d3a91349c53bdb15cf961ef73501
tianfahreza-pro <tianfahreza240@gmail.com>
2026-02-24 13:18:45 +0700

    Pertanyaan1_Percobaan3

--- P2/Mahasiswa19.java
+++ P2/Mahasiswa19.java
@@ -7,6 +7,16 @@ public class Mahasiswa19 {
     String kelas;
     double ipk;
 
+    public Mahasiswa19 () {
+    }
+
+    public Mahasiswa19 (String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
+
     void tampilkanInformasi() {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
