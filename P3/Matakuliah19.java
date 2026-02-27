80aab6d4ba26657e40d0629154d0c552697c8036
tianfahreza-pro <tianfahreza240@gmail.com>
2026-02-27 11:04:54 +0700

    Percobaan3

---
+++ P3/Matakuliah19.java
@@ -0,0 +1,15 @@
+package P3;
+
+public class Matakuliah19 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public Matakuliah19 (String kode, String nama, int sks, int jumlahJam) {
+    this.kode = kode;
+    this. nama = nama;
+    this.sks = sks;
+    this. jumlahJam = jumlahJam;
+    }
+}
