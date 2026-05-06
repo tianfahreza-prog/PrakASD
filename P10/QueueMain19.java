da559d6640c0042d2231dd96627ade39ceaba096
tianfahreza-pro <tianfahreza240@gmail.com>
2026-05-06 23:22:36 +0700

    Percobaan1

---
+++ P10/QueueMain19.java
@@ -0,0 +1,54 @@
+package P10;
+
+import java.util.Scanner;
+
+public class QueueMain19 {
+    public static void menu() {
+        System.out.println("Masukkan operasi yang diinginkan: ");
+        System.out.println("1. Enqueue");
+        System.out.println("2. Dequeue");
+        System.out.println("3. Print");
+        System.out.println("4. peek");
+        System.out.println("5. clear");
+        System.out.println("-----------------------");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan kapasitas queue: ");
+        int n = sc.nextInt();
+
+        Queue19 Q = new Queue19(n);
+
+        int pilih;
+
+        do {
+            menu();
+            pilih = sc.nextInt();
+            switch (pilih) {
+                case 1:
+                    System.out.println("Masukkan data baru: ");
+                    int dataMasuk = sc.nextInt();
+                    Q.Enqueue(dataMasuk);
+                    break;
+                case 2:
+                    int dataKeluar = Q.Dequeue();
+                    if (dataKeluar != 0) {
+                        System.out.println("Data yang dikeluarkan: " + dataKeluar);
+                        break;
+                    }
+                case 3:
+                    Q.print();;
+                    break;
+                case 4:
+                    Q.peek();
+                    break;
+                case 5:
+                    Q.clear();
+                    break;
+            }
+            
+        } while (pilih == 1 || pilih == 2 || pilih == 3 || pilih == 4 || pilih == 5);
+    }
+}
